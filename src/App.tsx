789afc728767b37f035df1b3a80453499f62055b
adityasuman2025 <adityasuman2025@gmail.com>
2023-03-06 14:15:43 +0530

    camera poc done

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,13 @@ import './App.css';
 import Camera from './Camera';
 
 function App() {
-    const [imgSrc, setImgSrc] = useState<string>("");
-
     return (
-        <div className="App">
-            <Camera
-                captureBtn = {
-                    <button>capture</button>
-                }
-                onCaptureClick={(e: any) => {
-                    const imgUrl = URL.createObjectURL(e);
-                    setImgSrc(imgUrl)
-                    console.log("imgUrl", imgUrl)
-                }}
-            />
-            <img src={imgSrc} />
-        </div>
+        <Camera
+            captureBtn={
+                <button>capture</button>
+            }
+            onSubmitClick={(imgs) => console.log("imgs", imgs)}
+        />
     )
 }
 
--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,21 +1,25 @@
-import React, { useRef, useEffect, ReactNode } from 'react';
+import React, { useRef, useEffect, ReactNode, useState } from 'react';
 
 const CAMERA_VIEW_DEFAULT_WIDTH = 560, CAMERA_VIEW_DEFAULT_HEIGHT = 373;
+const IMG_DIM = 48;
+
 interface CameraProps {
     cameraViewWidth?: number,
     cameraViewHeight?: number,
     captureBtnContClassName?: string,
     captureBtn?: string | ReactNode,
-    onCaptureClick?: (imgData: any) => void
+    onSubmitClick: (images: { [key: string]: any }[]) => void,
 }
 export default function Camera({
     cameraViewWidth = CAMERA_VIEW_DEFAULT_WIDTH,
     cameraViewHeight = CAMERA_VIEW_DEFAULT_HEIGHT,
     captureBtnContClassName = "",
     captureBtn = "",
-    onCaptureClick,
+    onSubmitClick,
 }: CameraProps) {
-    const cameraViewRef: any = useRef();
+    const cameraViewRef: any = useRef<any>();
+    const [images, setImages] = useState<{ [key: string]: any }[]>([]);
+    const [isCameraActive, setIsCameraActive] = useState<boolean>(false);
 
     useEffect(() => {
         (async () => {
@@ -25,13 +29,20 @@ export default function Camera({
                     console.log("device", device.label);
                     setDevice(device);
                 });
-            } catch { }
+            } catch { };
         })();
+
+        return () => {
+            // release the memory used by image url created by createObjectURL()
+            images.forEach(image => URL.revokeObjectURL(image.imgUrl));
+        }
     }, []);
 
     async function setDevice(device: any) {
         const { deviceId } = device || {};
         const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { deviceId } });
+
+        setIsCameraActive(true);
         cameraViewRef.current.srcObject = stream;
         cameraViewRef.current.play();
     }
@@ -43,24 +54,62 @@ export default function Camera({
 
         const ctx: any = canvasEle.getContext('2d');
         ctx.drawImage(cameraViewRef.current, 0, 0, cameraViewWidth, cameraViewHeight);
-        onCaptureClick && canvasEle.toBlob(onCaptureClick);
+        canvasEle.toBlob(function (e: any) {
+            const imgUrl = URL.createObjectURL(e);
+            setImages(prev => ([...prev, { imgUrl }]));
+        });
     }
 
     return (
         <>
-            <video
-                ref={cameraViewRef}
-                width={cameraViewWidth || CAMERA_VIEW_DEFAULT_WIDTH}
-                height={cameraViewHeight || CAMERA_VIEW_DEFAULT_HEIGHT}
-            />
+            {
+                isCameraActive ?
 
-            <div
-                role='button'
-                onClick={handleCaptureClick}
-                className={captureBtnContClassName}
-            >
-                {captureBtn || "capture"}
-            </div>
+                    <div className='cameraComp'>
+                        <video
+                            ref={cameraViewRef}
+                            width={cameraViewWidth || CAMERA_VIEW_DEFAULT_WIDTH}
+                            height={cameraViewHeight || CAMERA_VIEW_DEFAULT_HEIGHT}
+                        />
+
+                        <div className='footer'>
+                            {
+                                images.length ?
+                                    <>
+                                        {
+                                            images.map((img, idx) => (
+                                                <img
+                                                    key={idx}
+                                                    className='thumbImg'
+                                                    src={img.imgUrl}
+                                                    width={IMG_DIM}
+                                                    height={IMG_DIM}
+                                                    style={{ left: idx * IMG_DIM / 2 }}
+                                                />
+                                            ))
+                                        }
+                                        <div className='imgThumbCount' style={{ left: images.length * IMG_DIM / 2 }}>{images.length}</div>
+                                    </>
+                                    : null
+                            }
+
+                            <div className='footerItem'></div>
+                            <div className='footerItem'>
+                                <div
+                                    role='button'
+                                    onClick={handleCaptureClick}
+                                    className={`${captureBtnContClassName}`}
+                                >
+                                    {captureBtn || "capture"}
+                                </div>
+                            </div>
+                            <div className='footerItem'>
+                                <button onClick={() => { onSubmitClick && onSubmitClick(images) }}>submit</button>
+                            </div>
+                        </div>
+                    </div>
+                    : <div>Allow camera & microphone access</div>
+            }
         </>
     );
 }
