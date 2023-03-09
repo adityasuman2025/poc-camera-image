16e2142d4413b3834851bf1b802b093ee4a0f113
adityasuman2025 <adityasuman2025@gmail.com>
2023-03-09 12:06:09 +0530

    camera image capture done

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,16 @@
-import React, { useState, useRef, useCallback } from 'react'
+import React from 'react'
 import './App.css';
 import Camera from './Camera';
-import Cropper from './Cropper';
 
-const App = () => {
-    const [cropState, setCropState] = useState<any>()
-    const [img, setImg] = useState<any>()
-    const [inputKey, setInputKey] = useState<any>(0)
-    const cropperRef = useRef<any>()
-
-    const onDragStop = useCallback((s: any) => setCropState(s), [])
-    const onChange = useCallback((s: any) => setCropState(s), [])
-
-    const doSomething = async () => {
-        console.log(cropState)
-        try {
-            const res = await cropperRef.current.done({ preview: true })
-            console.log(res)
-        } catch (e) {
-            console.log('error', e)
-        }
-    }
-
-    const onImgSelection = async (e: any) => {
-        if (e.target.files && e.target.files.length > 0) {
-            // it can also be a http or base64 string for example
-            setImg(e.target.files[0])
-        }
-    }
-
+function App() {
     return (
-        <>
-            <Cropper
-                ref={cropperRef}
-                image={img}
-                onChange={onChange}
-                onDragStop={onDragStop}
-            />
-            <input
-                type='file'
-                key={inputKey}
-                onChange={onImgSelection}
-                accept='image/*'
-            />
-            <button onClick={doSomething}>Ho finito</button>
-        </>
+        <Camera
+            captureBtn={
+                <button>capture</button>
+            }
+            onSubmitClick={(imgs) => console.log("imgs", imgs)}
+        />
     )
 }
 
-
-// function App() {
-//     return (
-//         <Camera
-//             captureBtn={
-//                 <button>capture</button>
-//             }
-//             onSubmitClick={(imgs) => console.log("imgs", imgs)}
-//         />
-//     )
-// }
-
-export default App
+export default App;
